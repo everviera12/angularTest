6faec97ad957f77c9d9fe5480ee3e9c207dc743b
everviera12 <evertigre10@gmail.com>
2023-08-10 20:37:26 -0600

    angular learning project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { employeesComponent } from './employees/employees.component';
+import { EmployeComponent } from './employe/employe.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, employeesComponent, EmployeComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
