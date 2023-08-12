502e0687db75cd5f5be63a540eab2fca7dd9b557
EverViera12 <evertigre10@gmail.com>
2023-08-12 14:28:55 -0600

    test

--- src/app/employe/employe.component.ts
+++ src/app/employe/employe.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employe',
   templateUrl: './employe.component.html',
-  styleUrls: ['./employe.component.css']
+  styleUrls: ['./employe.component.css'],
 })
 export class EmployeComponent {
-
+  nombre = 'Victor Everardo Viera';
+  edad = 3;
+  ocupacion = "software engineer"
 }
